e9ce4eb98619de08d600793a34f35e8dd4b968c7
Justin Cano <jcano001@ucr.edu>
2014-12-16 00:06:24 -0800

    initial commit

---
+++ cine/settings.py
@@ -0,0 +1,110 @@
+"""
+Django settings for cine project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), ".."),
+)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY', None)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'djangobower',
+    'django_nvd3',
+    'cine',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'cine.urls'
+
+WSGI_APPLICATION = 'cine.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+# django-bower stuff
+BOWER_COMPONENTS_ROOT = os.path.join(PROJECT_ROOT, 'components')
+BOWER_INSTALLED_APPS = (
+    'd3#3.3.8',
+    'nvd3#1.1.12-beta',
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'djangobower.finders.BowerFinder',
+)
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
---
+++ cine/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+from bom import BOM
+
+
+def index(request):
+    #return HttpResponse("Hello, world. You're at cine.")
+    bom = BOM()
+
+    movies = bom.weekend_chart()
+
+    xdata = []
+    titles = []
+    ydata = []
+
+    for (i, m) in enumerate(movies):
+        xdata.append(i)
+        titles.append(str(m.title))
+        ydata.append(m.gross_val)
+
+    extra_serie = {"tooltip": {"y_start": "$", "y_end": ""}}
+
+    chartdata = {
+        'x': titles,
+        #'name1': 'Weekend Box Office Charts',
+        'y1': ydata, 'extra1': extra_serie
+        #'y2': ydata, 'extra2': extra_serie
+    }
+
+    for i in range(0, len(xdata)):
+        chartdata['name%d' % (i+1)] = titles[i]
+        chartdata['y%d' % (i+1)] = [ydata[i]]
+        chartdata['extra%d' % (i+1)] = extra_serie
+
+    charttype = "multiBarHorizontalChart"
+    data = {
+        'charttype': charttype,
+        'chartdata': chartdata,
+        'height': '100%', 'width': '100%'
+    }
+
+    return render_to_response('multibarhorizontalchart.html', data)
