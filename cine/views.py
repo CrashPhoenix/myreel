df3ccc151401be66a4ae671348233eae618d4c4b
Justin Cano <jcano001@ucr.edu>
2014-12-17 21:13:10 -0800

    populate movie chart list

--- cine/views.py
+++ cine/views.py
@@ -12,11 +12,13 @@ def index(request):
     xdata = []
     titles = []
     ydata = []
+    ranks = []
 
     for (i, m) in enumerate(movies):
         xdata.append(i+1)
         titles.append(str(m.title))
         ydata.append(m.gross_val)
+        ranks.append({'title':m.title, 'rank':m.rank})
 
     extra_serie = {"tooltip": {"y_start": "$", "y_end": ""}}
 
@@ -44,7 +46,8 @@ def index(request):
         'charttype': charttype,
         'chartdata': chartdata,
         'height': '80%', 'width': '100%',
-        'kw_extra': kw_extra
+        'kw_extra': kw_extra,
+        'movies': ranks
     }
 
     return render_to_response('multibarhorizontalchart.html', data)
