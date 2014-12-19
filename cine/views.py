f72c9f130e72af590861f7a0dad95e8c3b52f393
Justin Cano <jcano001@ucr.edu>
2014-12-18 22:18:16 -0800

    added line chart

--- cine/views.py
+++ cine/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response
 
 from bom import BOM, DAILY_CHART, WEEKEND_CHART, WEEKLY_CHART
 
+import time, random, datetime
+
 def index(request, title=DAILY_CHART):
     #return HttpResponse(str(title))
     bom = BOM(title)
@@ -27,7 +29,11 @@ def index(request, title=DAILY_CHART):
         xdata.append(i+1)
         titles.append(str(m.title))
         ydata.append(m.gross_val)
-        ranks.append({'title':m.title, 'rank':m.rank})
+        ranks.append({
+            'title': m.title,
+            'rank': m.rank,
+            'id': m.movie_id
+        })
 
     extra_serie = {"tooltip": {"y_start": "$", "y_end": ""}}
 
@@ -63,3 +69,45 @@ def index(request, title=DAILY_CHART):
     }
 
     return render_to_response('index.html', data)
+
+def movie(request, movie_id):
+    """
+    lineChart page
+    """
+    start_time = int(time.mktime(datetime.datetime(2012, 6, 1).timetuple()) * 1000)
+    nb_element = 150
+    xdata = range(nb_element)
+    xdata = map(lambda x: start_time + x * 1000000000, xdata)
+    ydata = [i + random.randint(1, 10) for i in range(nb_element)]
+    ydata2 = map(lambda x: x * 2, ydata)
+
+    tooltip_date = "%d %b %Y %H:%M:%S %p"
+    extra_serie1 = {
+        "tooltip": {"y_start": "", "y_end": " cal"},
+        "date_format": tooltip_date,
+        'color': '#a4c639'
+    }
+    extra_serie2 = {
+        "tooltip": {"y_start": "", "y_end": " cal"},
+        "date_format": tooltip_date,
+        'color': '#FF8aF8'
+    }
+    chartdata = {'x': xdata,
+                 'name1': 'series 1', 'y1': ydata, 'extra1': extra_serie1,
+                 'name2': 'series 2', 'y2': ydata2, 'extra2': extra_serie2}
+
+    charttype = "lineChart"
+    chartcontainer = 'linechart_container'  # container name
+    data = {
+        'charttype': charttype,
+        'chartdata': chartdata,
+        'chartcontainer': chartcontainer,
+        'extra': {
+            'x_is_date': True,
+            'x_axis_format': '%d %b %Y %H',
+            'tag_script_js': True,
+            'jquery_on_ready': False,
+        },
+        'height': '80%', 'width': '100%'
+    }
+    return render_to_response('movie.html', data)
