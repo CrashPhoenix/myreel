58756356ae2265a53ea75fb6eae8dc7320065634
Justin Cano <jcano001@ucr.edu>
2014-12-27 16:14:56 -0800

    removed x-tag-imports...cuz I couldn't get them to work 😔

--- myreel/settings.py
+++ myreel/settings.py
@@ -105,7 +105,6 @@ BOWER_INSTALLED_APPS = (
     'Polymer/paper-elements#latest',
     'PolymerLabs/polymer-ui-card#latest',
     'normalize.css',
-    'x-tag/x-tag-imports',
 )
 
 STATICFILES_FINDERS = (
